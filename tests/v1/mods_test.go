bc88e7ff01ba994e3fc49f951a6325da787f9b80
O3H <owen77stubbs@gmail.com>
2025-02-16 19:42:43 +0000

    Update mods_test.go

--- tests/v1/mods_test.go
+++ tests/v1/mods_test.go
@@ -15,11 +15,11 @@ const testModFileId = 1444
 func TestGetModByID(t *testing.T) {
 	mod, err := ModService.GetModByID(testModGame, testModId)
 	if err != nil {
-		t.Fatalf("error getting mod by id: %d\n%v", testModId, err)
+		t.Fatalf("failed to get mod by id: %d\n%v", testModId, err)
 	}
 
 	if mod == nil {
-		t.Fatalf("error getting mod by id: %d. mod is nil but no error was provided", testModId)
+		t.Fatalf("failed to get mod by id: %d. mod is nil but no error was provided", testModId)
 	}
 
 	//util.PrettyPrint(mod)
@@ -28,11 +28,11 @@ func TestGetModByID(t *testing.T) {
 func TestGetModChangelogs(t *testing.T) {
 	changelogs, err := ModService.GetModChangelogs(testModGame, testModId)
 	if err != nil {
-		t.Fatalf("error getting changelogs for mod: %d\n%v", testModId, err)
+		t.Fatalf("failed to get changelogs for mod: %d\n%v", testModId, err)
 	}
 
 	if changelogs == nil {
-		t.Fatalf("error getting changelogs for mod: %d. response was nil but no error was provided", testModId)
+		t.Fatalf("failed to get changelogs for mod: %d. response was nil but no error was provided", testModId)
 	}
 
 	//util.PrettyPrint(mod)
@@ -41,11 +41,11 @@ func TestGetModChangelogs(t *testing.T) {
 func TestGetModFiles(t *testing.T) {
 	files, err := ModService.GetModFiles(testModGame, testModId)
 	if err != nil {
-		t.Fatalf("error getting files for mod: %d\n%v", testModId, err)
+		t.Fatalf("failed to get files for mod: %d\n%v", testModId, err)
 	}
 
 	if files == nil {
-		t.Fatalf("error getting files for mod: %d. response was nil but no error was provided", testModId)
+		t.Fatalf("failed to get files for mod: %d. response was nil but no error was provided", testModId)
 	}
 
 	//util.PrettyPrint(mod)
@@ -54,11 +54,11 @@ func TestGetModFiles(t *testing.T) {
 func TestGetModFile(t *testing.T) {
 	file, err := ModService.GetModFileByID(testModGame, testModId, testModFileId)
 	if err != nil {
-		t.Fatalf("error getting file %d for mod: %d\n%v", testModFileId, testModId, err)
+		t.Fatalf("failed to get file %d for mod: %d\n%v", testModFileId, testModId, err)
 	}
 
 	if file == nil {
-		t.Fatalf("error getting file %d for mod: %d. response was nil but no error was provided", testModFileId, testModId)
+		t.Fatalf("failed to get file %d for mod: %d. response was nil but no error was provided", testModFileId, testModId)
 	}
 
 	//util.PrettyPrint(mod)
