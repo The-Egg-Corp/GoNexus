c51948c143079a8e01ec4499bb93a3a0b40c1063
O3H <owen77stubbs@gmail.com>
2025-02-17 12:34:46 +0000

    document client.go

--- v1/client.go
+++ v1/client.go
@@ -1,3 +1,6 @@
+// This package is for interacting with version 1.0 of the [NexusMods API].
+//
+// [NexusMods API]: https://app.swaggerhub.com/apis-docs/NexusMods/nexus-mods_public_api_params_in_form_data/1.0
 package v1
 
 import (
@@ -6,14 +9,23 @@ import (
 	"github.com/the-egg-corp/gonexus/util"
 )
 
+// TODO: Support Single Sign-On (SSO) for application use. The current client will only work for personal keys afaik?
+
+// The client that is used to send requests to the Nexus API. Since it holds the API key, it will usually be required by methods so they can return valid data -
+// though this is almost always done internally and you should rarely ever need to pass the client around except for when creating a new service.
+//
+// IMPORTANT:
+// While in most cases a personal API key is fine, it is recommended you learn about the rate-limits employed by NexusMods and read their
+// [Acceptable Use Policy]. For use in a public facing a application, you may be required to register your app and use the Single Sign-On (SSO) system instead.
+//
+// [Acceptable Use Policy]: https://help.nexusmods.com/article/114-api-acceptable-use-policy
 type NexusClient struct {
 	apiKey string
 }
 
-// NexusMods API Permalink:
-// https://app.swaggerhub.com/apis-docs/NexusMods/nexus-mods_public_api_params_in_form_data/1.0
-
-// Creates a new [NexusClient]
+// Creates a new [NexusClient] using the input API key. Before a client is instantiated, a request is made to validate the API key (see [VALIDATE_USER_ENDPOINT]).
+// In the case that it is invalid/empty, a nil client is returned with an appropriate error.
+// Otherwise, everything succeeded and the returned client is assumed to be valid.
 func NewNexusClient(key string) (*NexusClient, error) {
 	user, err := SendValidateUserRequest(key)
 	if err != nil {
